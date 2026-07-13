9549170f4614794a92b4b380df7aedb7a3bc4dba
Joaodaniel11telegram <pedrovasco263@gmail.com>
2026-07-12 22:23:55 -0300

    Add Home component with welcome message

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Bem-vindo ao Meu Site</h1>
+      <p>Confira nossos produtos e ofertas especiais.</p>
+    </div>
+  )
+}

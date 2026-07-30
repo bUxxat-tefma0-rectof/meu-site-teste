43085245cc3407d12aea1ecf9c5f129eb07e92bf
Joaodaniel11telegram <pedrovasco263@gmail.com>
2026-07-30 11:29:17 -0300

    Delete backend directory

--- backend/app/utils/admin_dependency.py
+++
@@ -1,9 +0,0 @@
-from fastapi import Depends, HTTPException
-from app.models.user import User
-from app.utils.auth_dependency import get_current_user
-
-
-def get_current_admin(current_user: User = Depends(get_current_user)) -> User:
-    if not current_user.is_admin:
-        raise HTTPException(status_code=403, detail="Acesso restrito a administradores")
-    return current_user
